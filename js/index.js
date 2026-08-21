1d967be56c77c2982f07899ea6c0af0681e306e3
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-21 15:36:56 -0500

    Add starting html, style, and functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+function operate(first, second, op) {}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
